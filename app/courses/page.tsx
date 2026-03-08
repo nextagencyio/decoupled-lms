2d412a634562622613574e200798a5f03b0cc88f
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import CourseCard from '../components/CourseCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Courses | Lms',
